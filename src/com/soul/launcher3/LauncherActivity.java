88566041a022667c3847cf755aeabd011f639eb8
soul <405557524@qq.com>
2017-08-29 10:38:43 +0800

    第一次提交

---
+++ src/com/soul/launcher3/LauncherActivity.java
@@ -0,0 +1,22 @@
+package com.soul.launcher3;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * * @author soul
+ *
+ * @项目名:Launcher3
+ * @包名: com.soul.launcher3
+ * @作者：祝明
+ * @描述：TODO
+ * @创建时间：2017/8/28 18:04
+ */
+public class LauncherActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
